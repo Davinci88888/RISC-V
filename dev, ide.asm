e642be6ccb669ef1d602757913552b9d6fae14d8
Davinci88888 <127519141+Davinci88888@users.noreply.github.com>
2023-03-10 20:17:56 +0600

    Add files via upload

---
+++ dev, ide.asm
@@ -0,0 +1,37 @@
+XOR=TIMExPASSED
+
+RESPOND,
+
+TO EVIL, 
+
+WITH GOOD
+
+SUM=x+1,
+XOR=x-1,
+2sC=x+2,
+2sM=x-2,
+TsC=x+3,
+TsM=x-3,
+
+TsC, x+3, TsM, x-3
+
+TC:x3
+TM:x3
+
+TREE, COMMAND;
+
+test eax, 1
+35 3D 41 4C 54 45 52 41 54 49 4F 4E 3F
+
+x=return.to;
+d=returned.to;
+
+AND
+*
+
+GOD_KNOWS_BEST!
+
+test rax, 1
+key= 44 41 56 45 2C 20 49 20 44 45 56
+
+DEVs_I_DEV_E_LOP_ED
